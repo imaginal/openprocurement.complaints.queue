8fffb53d2ec8eeb06bc22985cdb8e6198626949a
Volodymyr Flonts <flyonts@gmail.com>
2016-07-27 05:34:13 +0300

    update table structure add cancellation_json

--- openprocurement/complaints/queue/mysql.py
+++ openprocurement/complaints/queue/mysql.py
@@ -62,12 +62,16 @@ class ComplaintsToMySQL(ComplaintsClient):
                   complaint_id char(32) NOT NULL,
                   complaint_complaintID varchar(40) NOT NULL,
                   complaint_path varchar(80) NOT NULL,
-                  complaint_date varchar(40) NOT NULL,
+                  complaint_acceptance tinyint(1) default NULL,
+                  complaint_dateSubmitted varchar(40) default NULL,
+                  complaint_dateAccepted varchar(40) default NULL,
                   complaint_status varchar(40) NOT NULL,
                   complaint_json longblob NOT NULL,
+                  cancellation_json longblob default NULL,
+                  cancellation_dateDecision varchar(40) default NULL,
                   PRIMARY KEY (complaint_id),
                   KEY complaint_complaintID (complaint_complaintID),
-                  KEY complaint_date (complaint_date),
+                  KEY complaint_dateSubmitted (complaint_dateSubmitted),
                   KEY complaint_status (complaint_status),
                   KEY tender_procurementMethod (tender_procurementMethod)
                 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -79,7 +83,7 @@ class ComplaintsToMySQL(ComplaintsClient):
             self.execute_query(SQL)
 
     def update_skip_until(self):
-        self.execute_query("SELECT MAX(complaint_date) FROM {table_name}")
+        self.execute_query("SELECT MAX(complaint_dateSubmitted) FROM {table_name}")
         row = self.cursor.fetchone()
         if not row or not row[0]:
             return
@@ -91,35 +95,44 @@ class ComplaintsToMySQL(ComplaintsClient):
         logger.info("Update offset from database, set to '%s'", row_date)
         self.skip_until = row_date
 
-    def test_exists(self, complaint_id, complaint_date):
-        # 2016-04-13 complaint_date not updated, so always update complaint_json
-        return False
-        # the old fashioned way
-        self.execute_query(("SELECT complaint_date FROM {table_name} "+
-            "WHERE complaint_id=%s LIMIT 1"), (complaint_id,))
+    def test_exists(self, tender, complaint):
+        # don't update rows in terminal status
+        self.execute_query(("SELECT tender_status FROM {table_name} "+
+            "WHERE complaint_id=%s LIMIT 1"), (complaint.id, ))
         row = self.cursor.fetchone()
-        return row and row[0] == complaint_date
+        return row and row[0] == "cancelled"
 
-    def store(self, complaint, complaint_path, complaint_date):
+    def store(self, complaint, complaint_path):
         complaint_json = json.dumps(complaint)
-        self.execute_query(("INSERT INTO {table_name} "+
-            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) "+
-            "ON DUPLICATE KEY UPDATE "+
-            "tender_status=%s, complaint_date=%s, complaint_status=%s, complaint_json=%s"),
-           (complaint.tender.id,
-            complaint.tender.status,
-            complaint.tender.procurementMethod,
-            complaint.tender.procurementMethodType,
-            complaint.tender.get('mode', None), # optional
-            complaint.id,
-            complaint.complaintID,
-            complaint_path,
-            complaint_date,
-            complaint.status,
-            complaint_json,
-            # ON DUPLICATE KEY UPDATE
-            complaint.tender.status,
-            complaint_date,
-            complaint.status,
-            complaint_json))
+        insert_data = [
+            ('tender_id', complaint.tender.id),
+            ('tender_status', complaint.tender.status),
+            ('tender_procurementMethod', complaint.tender.procurementMethod),
+            ('tender_procurementMethodType', complaint.tender.procurementMethodType),
+            ('tender_mode', complaint.tender.get('mode', None)),
+            ('complaint_id', complaint.id),
+            ('complaint_complaintID', complaint.complaintID),
+            ('complaint_path', complaint_path),
+            ('complaint_acceptance', complaint.get('acceptance', None)),
+            ('complaint_dateSubmitted', complaint.get('dateSubmitted', None)),
+            ('complaint_dateAccepted',  complaint.get('dateAccepted', None)),
+            ('complaint_status', complaint.status),
+            ('complaint_json', complaint_json),
+        ]
+        update_data = [
+            ('tender_status', complaint.tender.status),
+            ('complaint_status', complaint.status),
+            ('complaint_acceptance', complaint.get('acceptance', None)),
+            ('complaint_dateAccepted', complaint.get('dateAccepted', None)),
+            ('complaint_json', complaint_json),
+        ]
+        insert_cols = ", ".join([k for k,_ in insert_data])
+        insert_fmts = ", ".join(["%s" for _ in insert_data])
+        update_cols = ", ".join([k+"=%s" for k,_ in update_data])
+        values_args = [v for _,v in insert_data+update_data]
+
+        SQL = "INSERT INTO %s (%s) VALUES (%s) ON DUPLICATE KEY UPDATE %s" % (
+            self.table_name, insert_cols, insert_fmts, update_cols)
+        #logger.debug(SQL)
+        self.execute_query(SQL, values_args)
         self.dbcon.commit()
